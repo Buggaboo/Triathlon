f3593038c0475a26f8c70e64f1299878e4706dea
Buggaboo <buggasnoo@gmail.com>
2009-11-28 20:44:56 +0100

    added new shell script to downloaded required files

---
+++ deps/slackware/get-src.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e -x
+
+GREP=$(which grep)
+WGET=$(which wget)
+
+for d in $(ls -d */)
+do
+  cd $d  
+  $WGET -c -nd -nH `$GREP "DOWNLOAD=" *.info | sed 's|DOWNLOAD="\(.*\)"|\1|'`
+  cd ..
+done
+
+
+
