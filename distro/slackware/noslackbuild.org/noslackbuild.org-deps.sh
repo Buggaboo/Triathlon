adfe21a1cc5649d90295d93226962c97366a95a1
Buggaboo <buggasnoo@gmail.com>
2009-11-28 15:03:25 +0100

    new files added

--- distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
+++ distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
@@ -7,3 +7,6 @@ wget -c -nd -nH \
   http://downloads.sourceforge.net/project/mdp-toolkit/mdp-toolkit/2.5/MDP-2.5.tar.gz?use_mirror=switch
 # TODO - make slackbuild that also builds the python bindings
   http://prdownloads.sourceforge.net/fann/fann-2.0.0.tar.bz2?download
+
+mv MDP* mdp/
+mv fann* fann/
