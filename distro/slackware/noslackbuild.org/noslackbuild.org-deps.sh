e71c8e4b39cfdc335ff8b637f6250df7d9423cbb
Buggaboo <buggasnoo@gmail.com>
2009-11-28 15:27:07 +0100

    updated TODO and LOG

--- distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
+++ distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# TODO
+# - make slackbuild that installs this thing
+# - make slackbuild that also builds the python bindings
 
 # These files require a slackbuild file to build
 wget -c -nd -nH \
-# TODO - make slackbuild that installs this thing
-  http://downloads.sourceforge.net/project/mdp-toolkit/mdp-toolkit/2.5/MDP-2.5.tar.gz?use_mirror=switch
-# TODO - make slackbuild that also builds the python bindings
+  http://downloads.sourceforge.net/project/mdp-toolkit/mdp-toolkit/2.5/MDP-2.5.tar.gz?use_mirror=switch \
   http://prdownloads.sourceforge.net/fann/fann-2.0.0.tar.bz2?download
 
-mv MDP* mdp/
-mv fann* fann/
+mv MDP-* mdp/
+mv fann-* fann/
