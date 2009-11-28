e99f6cc98ec19985e3a651c021363d62723cb819
Buggaboo <buggasnoo@gmail.com>
2009-11-28 14:42:02 +0100

    accidentally downloaded a .asc file

--- distro/slackware/get-all-slackware-deps.sh
+++ distro/slackware/get-all-slackware-deps.sh
@@ -6,6 +6,7 @@
 
 # These scripts require arch to be modified (x86 or x86_64)
 # zum Beispiel: sh ARCH=x86_64 asdf.SlackBuild
+# Build and install in the some sequence as it is downloaded
 
 $(which wget) -c -nd -nH \
   http://slackbuilds.org/slackbuilds/13.0/libraries/wxGTK.tar.gz \
@@ -16,18 +17,18 @@ $(which wget) -c -nd -nH \
   http://downloads.sourceforge.net/pyopengl/PyOpenGL-3.0.0a5.tar.gz \
   http://slackbuilds.org/slackbuilds/13.0/libraries/python-xlib.tar.gz \
   http://downloads.sourceforge.net/python-xlib/python-xlib-0.14.tar.gz \
-  http://slackbuilds.org/slackbuilds/13.0/development/numpy.tar.gz \
-  http://downloads.sourceforge.net/numpy/numpy-1.3.0.tar.gz \
   http://slackbuilds.org/slackbuilds/13.0/libraries/pyusb.tar.gz \
   http://downloads.sourceforge.net/pyusb/pyusb-0.4.1.tar.gz \
-  http://slackbuilds.org/slackbuilds/13.0/academic/scipy.tar.gz \
-  http://downloads.sourceforge.net/scipy/scipy-0.7.0.tar.gz \
   http://slackbuilds.org/slackbuilds/13.0/libraries/fftw.tar.gz \
   ftp://ftp.fftw.org/pub/fftw/fftw-3.2.1.tar.gz \
   http://slackbuilds.org/slackbuilds/13.0/libraries/blas.tar.gz \
   http://www.netlib.org/blas/blas.tgz \
-  http://slackbuilds.org/slackbuilds/13.0/libraries/lapack.tar.gz.asc \
-  http://www.netlib.org/lapack/lapack.tgz
+  http://slackbuilds.org/slackbuilds/13.0/libraries/lapack.tar.gz \
+  http://www.netlib.org/lapack/lapack.tgz \
+  http://slackbuilds.org/slackbuilds/13.0/development/numpy.tar.gz \
+  http://downloads.sourceforge.net/numpy/numpy-1.3.0.tar.gz \
+  http://slackbuilds.org/slackbuilds/13.0/academic/scipy.tar.gz \
+  http://downloads.sourceforge.net/scipy/scipy-0.7.0.tar.gz
 
 # These scripts require some minor modification, like version number etc.
 wget -c -nd -nH \
--- distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
+++ distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
@@ -3,7 +3,7 @@
 
 # These files require a slackbuild file to build
 wget -c -nd -nH \
+# TODO - make slackbuild that installs this thing
   http://downloads.sourceforge.net/project/mdp-toolkit/mdp-toolkit/2.5/MDP-2.5.tar.gz?use_mirror=switch
-# TODO
-# - make slackbuild that also builds the python bindings
+# TODO - make slackbuild that also builds the python bindings
   http://prdownloads.sourceforge.net/fann/fann-2.0.0.tar.bz2?download
