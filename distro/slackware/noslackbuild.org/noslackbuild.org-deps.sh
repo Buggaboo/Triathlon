ab059d44438199add6906930645c32cf4ed31634
Buggaboo <buggasnoo@gmail.com>
2009-11-28 14:07:07 +0100

    Adding script to download slackbuilds

---
+++ distro/slackware/noslackbuild.org/noslackbuild.org-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+# These files require a slackbuild file to build
+wget -c -nd -nH \
+  http://downloads.sourceforge.net/project/mdp-toolkit/mdp-toolkit/2.5/MDP-2.5.tar.gz?use_mirror=switch
+# TODO
+# - make slackbuild that also builds the python bindings
+  http://prdownloads.sourceforge.net/fann/fann-2.0.0.tar.bz2?download
